2e5323d07f523aa3ae1eaea5c9107228284a4b5e
Darko Filipovski <darko.filipovski@netcetera.com>
2025-12-15 18:00:09 +0100

    Init project
    (cherry picked from commit a6d96dab421f4fbf7256c5fbb012db2db73c298d)

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
+
+@SpringBootApplication
+public class Application {
+
+  private static final Logger log = LoggerFactory.getLogger(Application.class);
+  
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
